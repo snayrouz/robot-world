4ec30869b55ef32d0f971b5ed54fcbc0eaa768c7
snayrouz <samuel.nayrouz@gmail.com>
2017-08-15 17:11:45 -0600

    Initial commit: add new erb and methods to robot

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -2,9 +2,29 @@ require_relative '../models/robot.rb'
 
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+  set :method_override, true
 
-  get '/robots' do
+  get '/' do
     erb :index
   end
-end
 
+  get '/robots' do
+    @robots = Robot.all
+    erb :index
+  end
+
+  get '/robots/new' do
+    erb :new
+  end
+
+  post '/robots' do
+    robot = Robot.new(params[:robot])
+    robot.save
+    redirect '/robots'
+  end
+
+  not_found do
+    erb :error
+  end
+
+end
--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,8 +1,33 @@
 require 'sqlite3'
 
 class Robot
-  def initialize(robot_params)
+  attr_reader :id, :name, :city, :state, :department, :database
+  def initialize(params)
+    @id = params["id"]
+    @name = params["name"]
+    @city = params["city"]
+    @state = params["state"]
+    @department = params["department"]
     @database = SQLite3::Database.new('db/robot_world_development.db')
     @database.results_as_hash = true
+  end
+
+  def save
+    @database.execute("INSERT INTO robots (name, city, state, department) VALUES (?, ?, ?, ?)", @name, @city, @state, @department)
+  end
+
+  def self.all
+  end
+
+  def self.database
+  end
+
+  def self.find(id)
+  end
+
+  def self.update(id, params)
+  end
+
+  def self.destroy(id)
   end
 end
